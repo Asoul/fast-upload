04f70c51e406d9ee16c3d2534907ee79c8b7919a
Asoul <b00902036@ntu.edu.tw>
2015-01-24 00:10:34 +0800

    1.1.0

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
 # fast-upload
 
-## Spec
-Page is extradinary simple, so very fast
+這是一個極簡的檔案上傳網站，用 nodejs 寫的，前端頁面超簡單，所以載入速度超快。
 
-## Usage
-should change the function in multer/index.js
+## 安裝使用方法
 
-line 101:
+1. <code>git clone https://github.com/Asoul/fast-upload.git</code>
+
+2. 把原本 `multer/index.js` 裡面的 code 改一下：
+3. 記得要開 `redis-server`
+
+>line 101:<br/>
+<code>
 var proceed = options.onFileUploadStart(file);
-to
+</code>
+<br/>變成<br/>
+<code>
 var proceed = options.onFileUploadStart(file, res);
+</code>
+
+>
+
+
+## 更新進度
+
+#### 2015/01/23
+1. 檔案上傳後會自動改檔名，下載後再改回來
+2. 用 redis 當 Database，所以 server 重開舊的紀錄都在
+3. 新增 404 not found 頁面
+4. 加了 favicon
+5. server log 統一格式
+
+## TODO
+1. 上傳進度條
+2. 上次砍檔時間
+3. 歷史上傳資料
+4. favicon 要改 url
--- app.js
+++ app.js
@@ -8,32 +8,93 @@ var server  = http.createServer(app);
 var fs      = require('fs');
 var multer  = require('multer');
 
-/* Confuguration */
-app.use(express.compress());
-app.use(express.static(require("path").join(__dirname, 'public')));
-app.set('view engine', 'ejs'); // set up ejs for templating
+var redis   = require('redis');
+var db      = redis.createClient('6379', '127.0.0.1');
 
-app.get('/', function(req, res){
-  res.render('index');
+
+/* Confuguration */
+app.set('view engine', 'ejs'); // set up ejs for templating
+db.on("error", function(error) {
+    console.log(error);
 });
+
+var file_lists = {};
 
 /* File Transfer */
 
 app.use(multer({
   dest: __dirname + '/public/uploads/',
   onFileUploadStart: function (file, res) {
-    res.send(file.name+file.originalname)
-    console.log(file.fieldname + ' is starting ...' + file.name);
+    // discard the file extension for handling
+    var newname = file.name;
+    var ext;
+
+    if (newname.indexOf('.') > 0) { ext = '.' + newname.split('.').slice(-1)[0]; }
+    else { ext = ''; }
+    newname = newname.replace(ext, '');
+
+    db.set(newname, file.originalname);
+    res.send(JSON.stringify({newname: newname, oldname: file.originalname}));
+    
+    console.log((new Date()).toLocaleString(), '[START] ', file.originalname + ' -> ' + file.name);
   },
   onFileUploadComplete: function (file) {
-    console.log(file.originalname + ' uploaded to  ' + file.path);
+    console.log((new Date()).toLocaleString(), '[FINIH] ', file.originalname + ' -> ' + file.name);
   }
 }));
 
-/* API Functions */
+/* Routers */
 
-app.post('/upload', function(req, res) {
-  // res.send('Yo');
+app.get('/', function(req, res){
+  res.render('index');
+  console.log((new Date()).toLocaleString(), '[NEWON] ');
+});
+
+app.get('/favicon', function(req, res){
+  res.sendFile(__dirname+'/public/favicon.ico');
+});
+
+app.get('/uploads/:token', function(req, res){
+  var token = req.params.token;
+  var ext, oldname;
+  db.get(token, function(err, reply) {
+    // reply is null when the key is missing
+    if (reply == null) {
+      res.status = 404;
+      res.render('error/404', { title:'404: Page not found', error: '404: Page not found', url: req.url });
+    }
+    oldname = reply;
+    var ext;
+    if (oldname.indexOf('.') > 0) { ext = '.' + oldname.split('.').slice(-1)[0]; }
+    else { ext = ''; }
+    console.log((new Date()).toLocaleString(), '[DONLO] ', token + ' -> ' + oldname);
+    res.download(__dirname+'/public/uploads/'+token+ext, oldname);
+  });
+});
+
+
+// error handling middleware. Because it's
+// below our routes, you will be able to
+// "intercept" errors, otherwise Connect
+// will respond with 500 "Internal Server Error".
+app.get('*', function(req, res){
+  console.log((new Date()).toLocaleString(), '[ERROR] ', req.url);
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
 });
 
 server.listen(port, function(){
